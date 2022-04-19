b6be346c1191bdc180237e4923433baacfa5bb51
Daniel Griffin <dgriffin@sealworks.com>
2022-04-19 16:16:18 -0400

    added validation and creation tests for Post and Comment models

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -12,4 +12,22 @@ RSpec.describe 'Comment', type: :model do
     comment = Comment.new(name: "Charles Mingus", comment_text: nil)
     expect(comment).to_not be_valid
   end
+
+  it 'is not valid without a Post id' do
+    comment = Comment.new(post_id: nil, name: "Gilfoyle", comment_text: "stallions")
+    expect(comment).to_not be_valid
+  end
+
+  context 'comment creation' do
+    it 'is valid with a Post id, name, and comment text' do
+      post = Post.create(title: "test", content: "interesting")
+      comment = Comment.new(post_id: post.id, name: "Gilfoyle", comment_text: "stallions")
+      expect(comment).to be_valid
+    end
+
+    it 'can be created with a Post id, name, and comment text' do
+      post = Post.create(title: "test", content: "interesting")
+      expect{Comment.create(post_id: post.id, name: "Gilfoyle", comment_text: "stallions")}.to change{Comment.count}.by(1)
+    end
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,13 +2,22 @@ require 'rails_helper'
 
 RSpec.describe 'Post', type: :model do
 
-  it 'is not valid without a title' do   #
+  it 'is not valid without a title' do
     post = Post.new(title: nil)
     expect(post).to_not be_valid
   end
 
-  it 'is not valid without content' do   #
+  it 'is not valid without content' do
     post = Post.new(title: "test", content: nil)
     expect(post).to_not be_valid
+  end
+
+  it 'is valid with a title and content' do
+    post = Post.new(title: "test", content: "interesting")
+    expect(post).to be_valid
+  end
+
+  it 'can be created with a title and content' do
+    expect{Post.create(title: "test", content: "let me be created")}.to change{Post.count}.by(1)
   end
 end
